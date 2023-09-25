0755d04b97a840884698cf70c8efe75c94e60ca0
Dola Naga Raju <101490408+deanraaj@users.noreply.github.com>
2023-09-25 20:20:55 +0530

    Update Question_17.java

--- Patterns/Question_17.java
+++ Patterns/Question_17.java
@@ -1,3 +1,35 @@
 public class Question_17 {
-    
+    public static void main(String[] args) {
+        int n = 7;
+
+        int space = 1;
+        int star = n/2;
+
+        for(int row =1; row<=n; row++){
+
+            for(int i=1; i<=star; i++){
+                System.out.print("* ");
+            }
+
+            for(int i=1; i<=space; i++){
+                System.out.print("  ");
+            }
+
+            for(int i=1; i<=star; i++){
+//                if(row ==1 && i==star) continue;
+//                if(row ==n && i==star) continue;
+                System.out.print("* ");
+            }
+
+            if(row <= n/2){
+                space+=2;
+                star--;
+            }else {
+                space-=2;
+                star++;
+            }
+            System.out.println();
+
+        }
+    }
 }
